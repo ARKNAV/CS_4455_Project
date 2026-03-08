f6b1791fc4894da36f3173b02e2435df198267df
ARKNAV <122695841+ARKNAV@users.noreply.github.com>
2026-03-08 18:05:35 -0400

    added scripts for npc behavior, will change in Unity and implement specific disguise behavior after Monday meeting

---
+++ Assets/Scenes/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
+    public static void TriggerLose()
+    {
+        if (Instance != null)
+        {
+            Instance.ReloadCurrentScene();
+            return;
+        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void ReloadCurrentScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
