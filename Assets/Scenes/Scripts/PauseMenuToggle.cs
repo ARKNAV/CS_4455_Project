296c51cce3dd0fb5d112af7542171adc691a932d
amcintyre <amcintyre40@gatech.edu>
2026-03-10 17:20:06 -0400

    Basic UI

---
+++ Assets/Scenes/Scripts/PauseMenuToggle.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+//used this from m3
+[RequireComponent(typeof(CanvasGroup))]
+public class PauseMenuToggle : MonoBehaviour
+{
+    private CanvasGroup canvasGroup;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //canvasGroup=GetComponent<CanvasGroup>();
+    }
+
+    void Awake()
+    {
+        canvasGroup=GetComponent<CanvasGroup>();
+        if(canvasGroup == null)
+        {
+            print("Canvas not found");
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.escapeKey.wasPressedThisFrame) {
+            if (canvasGroup.interactable) {
+                canvasGroup.interactable = false;
+                canvasGroup.blocksRaycasts = false;
+                canvasGroup.alpha = 0f;
+                Time.timeScale = 1f;
+            } else {
+                canvasGroup.interactable = true;
+                canvasGroup.blocksRaycasts = true;
+                canvasGroup.alpha = 1f;
+                Time.timeScale = 0f;
+            }
+        }
+    }
+}
