6cab23b8a35665e5d1b0448ad196b976517445d2
ARKNAV <122695841+ARKNAV@users.noreply.github.com>
2026-03-13 14:44:56 -0400

    animation, investigate, chase all work for NPCs; need to just attach to more NPCs other than tutorial and add disguises now

---
+++ Assets/Scripts/GuardAnimatorController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent)), RequireComponent(typeof(Animator))]
+public class GuardAnimatorController : MonoBehaviour
+{
+    [SerializeField] string speedParam = "speed";
+
+    NavMeshAgent agent;
+    Animator animator;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float speed = agent.velocity.magnitude;
+        animator.SetFloat(speedParam, speed);
+    }
+}
