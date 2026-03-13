e17b3a617b4ecee666db5a25705e713efaa3f176
amcintyre <amcintyre40@gatech.edu>
2026-03-13 03:51:37 -0400

    Options fleshed out on title screen

--- Assets/Scripts/PauseMenuToggle.cs
+++ Assets/Scripts/PauseMenuToggle.cs
@@ -37,4 +37,19 @@ public class PauseMenuToggle : MonoBehaviour
             }
         }
     }
+
+    public void MenuToggle()
+    {
+        if (canvasGroup.interactable) {
+                canvasGroup.interactable = false;
+                canvasGroup.blocksRaycasts = false;
+                canvasGroup.alpha = 0f;
+                Time.timeScale = 1f;
+            } else {
+                canvasGroup.interactable = true;
+                canvasGroup.blocksRaycasts = true;
+                canvasGroup.alpha = 1f;
+                Time.timeScale = 0f;
+            }
+    }
 }
